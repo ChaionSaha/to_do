55a88ca6273aed2faa457ee19a3c25803b273c56
Chaion-Saha <90419674+Chaion-Saha@users.noreply.github.com>
2022-07-28 00:08:31 +0600

    new design added

--- script/index.js
+++ script/index.js
@@ -1,6 +1,39 @@
-'use strict'
+"use strict";
 
-const timeContainer = document.querySelector('.time');
+const cursors = document.querySelectorAll(".cursor");
+const cursorDiv = document.querySelector(".cursor-div");
+const timeContainer = document.querySelector(".time");
 
-let time = new Date();
-console.log(Date.parse(time));
+setInterval(() => {
+	let time = new Date();
+	timeContainer.innerHTML = "";
+	const html = `<span>${time.toLocaleString("default", {
+		timeStyle: "medium",
+	})}</span>
+    <span>Today is ${time.toLocaleString("default", {
+		weekday: "long",
+	})}</span>${time.toLocaleString("default", { dateStyle: "long" })} <span>`;
+
+	timeContainer.innerHTML = html;
+}, 1000);
+
+document.addEventListener("mousemove", (e) => {
+	cursors.forEach((cursor) => {
+		cursor.style.top = `${e.clientY}px`;
+		cursor.style.left = `${e.clientX}px`;
+	});
+
+});
+
+document.querySelectorAll(".cursor-hide").forEach((cursor) => {
+	cursor.addEventListener("mouseenter", (e) => {
+		cursorDiv.classList.add("hide");
+	});
+});
+document.querySelectorAll(".cursor-hide").forEach((cursor) => {
+	cursor.addEventListener("mouseleave", (e) => {
+		cursorDiv.classList.remove("hide");
+	});
+});
+
+localStorage.setItem("x", "Fuck you");
